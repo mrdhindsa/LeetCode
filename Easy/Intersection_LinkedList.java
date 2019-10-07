96b341cb05b1403699eb55e53429f14cdfbc67ae
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-06 20:55:31 -0700

    Update Intersection_LinkedList.java

--- Easy/Intersection_LinkedList.java
+++ Easy/Intersection_LinkedList.java
@@ -1,17 +1,17 @@
 public ListNode brute_getIntersectionNode(ListNode headA, ListNode headB) { // O(n*n) runtime O(1) time
-  if(headA == null || headB == null) return null;
-  ListNode temp1 = headA, temp2 = headB;
-  while(temp1 != null){
-    while(temp2 != null){
-      if(temp1 == temp2){
-        return temp1;
-      }
-      temp2 = temp2.next;
+    if(headA == null || headB == null) return null;
+    ListNode temp1 = headA, temp2 = headB;
+    while(temp1 != null){
+        while(temp2 != null){
+            if(temp1 == temp2){
+                return temp1;
+            }
+            temp2 = temp2.next;
+        }
+        temp1 = temp1.next;
+        temp2 = headB;
     }
-    temp1 = temp1.next;
-    temp2 = headB;
-  }
-  return null; // Unreachable
+    return null; // Unreachable
 } 
 
 // Optimized time
